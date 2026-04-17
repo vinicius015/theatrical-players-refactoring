db2eb14742ea721800e534ccba5444bd653f6303
Vinicius Amaral <vinicius.santosama@gmail.com>
2026-04-17 16:12:54 -0300

    Tests: Fixed broken tests

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -12,33 +12,45 @@ namespace TheatricalPlayersRefactoringKata.Tests
         [Fact]
         public Task test_statement_example()
         {
-            var plays = new Dictionary<string, Play>();
-            plays.Add("hamlet", new Play("Hamlet", "tragedy"));
-            plays.Add("as-like", new Play("As You Like It", "comedy"));
-            plays.Add("othello", new Play("Othello", "tragedy"));
+            var plays = new Dictionary<string, Play>
+                {
+                    { "hamlet", new Play("Hamlet", PlayType.Tragedy) },
+                    { "as-like", new Play("As You Like It", PlayType.Comedy) },
+                    { "othello", new Play("Othello", PlayType.Tragedy) }
+                };
 
-            Invoice invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40)});
-            
-            StatementPrinter statementPrinter = new StatementPrinter();
+            Invoice invoice = new("BigCo", new List<Performance>
+                {
+                    new("hamlet", 55),
+                    new("as-like", 35),
+                    new("othello", 40)
+                });
+
+            StatementPrinter statementPrinter = new();
             var result = statementPrinter.Print(invoice, plays);
 
             return Verifier.Verify(result);
         }
+
         [Fact]
         public void test_statement_with_new_play_types()
         {
-            var plays = new Dictionary<string, Play>();
-            plays.Add("henry-v", new Play("Henry V", "history"));
-            plays.Add("as-like", new Play("As You Like It", "pastoral"));
+            var plays = new Dictionary<string, Play>
+                {
+                    { "henry-v", new Play("Henry V", (PlayType)999) },
+                    { "as-like", new Play("As You Like It", (PlayType)998) }
+                };
 
-            Invoice invoice = new Invoice("BigCoII", new List<Performance>{new Performance("henry-v", 53),
-                new Performance("as-like", 55)});
-            
-            StatementPrinter statementPrinter = new StatementPrinter();
+            Invoice invoice = new("BigCoII", new List<Performance>
+                {
+                    new("henry-v", 53),
+                    new("as-like", 55)
+                });
 
-            Assert.Throws<Exception>(() => statementPrinter.Print(invoice, plays));
+            StatementPrinter statementPrinter = new();
+
+            var exception = Assert.Throws<ArgumentException>(() => statementPrinter.Print(invoice, plays));
+            Assert.Equal("Unknown play type: 999", exception.Message);
         }
     }
 }
